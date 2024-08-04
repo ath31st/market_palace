7da61fe1859ff4d1ef96f81847cbd9daba2600c9
ath31st <ath31st@yandex.ru>
2024-08-04 22:40:16 +0300

    init project (full-stack simple marketplace)

---
+++ backend/build.gradle.kts
@@ -0,0 +1,52 @@
+val kotlin_version: String by project
+val logback_version: String by project
+val exposed_version: String by project
+val sqlite_version: String by project
+val auth0_version: String by project
+val bcrypt_version: String by project
+val dotenv_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.0.0"
+    id("io.ktor.plugin") version "2.3.12"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
+}
+
+group = "doma.sidim"
+version = "0.0.1"
+
+application {
+    mainClass.set("doma.sidim.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-auth-jvm")
+    implementation("io.ktor:ktor-server-auth-jwt")
+    implementation("io.ktor:ktor-server-auto-head-response-jvm")
+    implementation("io.ktor:ktor-server-resources-jvm")
+    implementation("io.ktor:ktor-server-host-common-jvm")
+    implementation("io.ktor:ktor-server-status-pages-jvm")
+    implementation("io.ktor:ktor-server-cors-jvm")
+    implementation("io.ktor:ktor-server-default-headers-jvm")
+    implementation("io.ktor:ktor-server-call-logging-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.xerial:sqlite-jdbc:$sqlite_version")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("com.auth0:java-jwt:$auth0_version")
+    implementation("at.favre.lib:bcrypt:$bcrypt_version")
+    implementation("io.github.cdimascio:dotenv-kotlin:$dotenv_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
