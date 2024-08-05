59a1333ffb80f033ee16738658877bfb5e8b49ee
ath31st <ath31st@yandex.ru>
2024-08-05 06:32:44 +0300

    reconfigurate added plugins v3

--- backend/src/main/kotlin/doma/sidim/plugins/Serialization.kt
+++ backend/src/main/kotlin/doma/sidim/plugins/Serialization.kt
@@ -3,16 +3,14 @@ package doma.sidim.plugins
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
-    }
-    routing {
-        get("/json/kotlinx-serialization") {
-            call.respond(mapOf("hello" to "world"))
-        }
+        json(Json {
+            ignoreUnknownKeys = true
+            prettyPrint = true
+            isLenient = true
+        })
     }
 }
