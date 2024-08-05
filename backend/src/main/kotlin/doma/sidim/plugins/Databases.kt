d331e4c79108c0f8160d09832c4156bb7050bf5d
ath31st <ath31st@yandex.ru>
2024-08-05 06:32:30 +0300

    reconfigurate added plugins v1

--- backend/src/main/kotlin/doma/sidim/plugins/Databases.kt
+++ backend/src/main/kotlin/doma/sidim/plugins/Databases.kt
@@ -5,15 +5,16 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.Database
 
 fun Application.configureDatabases() {
     val database = Database.connect(
-        url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
+        url = "jdbc:sqlite:./market.db",
         user = "root",
-        driver = "org.h2.Driver",
+        driver = "org.sqlite.JDBC",
         password = ""
     )
+
     val userService = UserService(database)
     routing {
         // Create user
