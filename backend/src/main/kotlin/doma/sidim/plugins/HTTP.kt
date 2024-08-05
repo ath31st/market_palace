75f94a3b8e9be10ab8ddf4ef707e083e79cc59d6
ath31st <ath31st@yandex.ru>
2024-08-05 06:32:39 +0300

    reconfigurate added plugins v2

--- backend/src/main/kotlin/doma/sidim/plugins/HTTP.kt
+++ backend/src/main/kotlin/doma/sidim/plugins/HTTP.kt
@@ -3,16 +3,16 @@ package doma.sidim.plugins
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.response.*
+import io.ktor.server.plugins.defaultheaders.*
 
 fun Application.configureHTTP() {
     install(CORS) {
-        allowMethod(HttpMethod.Options)
-        allowMethod(HttpMethod.Put)
-        allowMethod(HttpMethod.Delete)
-        allowMethod(HttpMethod.Patch)
-        allowHeader(HttpHeaders.Authorization)
-        allowHeader("MyCustomHeader")
-        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+        allowHeaders { true }
+        HttpMethod.DefaultMethods.forEach { allowMethod(it) }
+        allowNonSimpleContentTypes = true
+        anyHost()
+    }
+    install(DefaultHeaders) {
+        header("X-Engine", "Ktor")
     }
 }
--- backend/src/main/kotlin/doma/sidim/plugins/Routing.kt
+++ backend/src/main/kotlin/doma/sidim/plugins/Routing.kt
@@ -4,11 +4,13 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.autohead.*
 import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.resources.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     install(AutoHeadResponse)
+    install(Resources)
     install(StatusPages) {
         exception<Throwable> { call, cause ->
             call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
