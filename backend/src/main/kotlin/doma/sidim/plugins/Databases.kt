6db5c6dc0485492c8f06a2f1aaab13032b01c651
ath31st <ath31st@yandex.ru>
2024-08-07 09:51:36 +0300

    update Databases.kt plugin, delete extra code

--- backend/src/main/kotlin/doma/sidim/plugins/Databases.kt
+++ backend/src/main/kotlin/doma/sidim/plugins/Databases.kt
@@ -1,11 +1,10 @@
 package doma.sidim.plugins
 
-import io.ktor.http.*
+import doma.sidim.model.Users
 import io.ktor.server.application.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.configureDatabases() {
     val database = Database.connect(
@@ -15,39 +14,8 @@ fun Application.configureDatabases() {
         password = ""
     )
 
-    val userService = UserService(database)
-    routing {
-        // Create user
-        post("/users") {
-            val user = call.receive<ExposedUser>()
-            val id = userService.create(user)
-            call.respond(HttpStatusCode.Created, id)
-        }
-
-        // Read user
-        get("/users/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-            val user = userService.read(id)
-            if (user != null) {
-                call.respond(HttpStatusCode.OK, user)
-            } else {
-                call.respond(HttpStatusCode.NotFound)
-            }
-        }
-
-        // Update user
-        put("/users/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-            val user = call.receive<ExposedUser>()
-            userService.update(id, user)
-            call.respond(HttpStatusCode.OK)
-        }
-
-        // Delete user
-        delete("/users/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-            userService.delete(id)
-            call.respond(HttpStatusCode.OK)
-        }
+    transaction(database) {
+        SchemaUtils.create(Users)
+        //PreparedData.insertInitialData()
     }
 }
