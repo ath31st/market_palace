78f1939ac12d66894bef98d679f6c65d56c8cad9
ath31st <ath31st@yandex.ru>
2024-08-07 15:46:44 +0300

    add OrderRepository with crud methods

---
+++ backend/src/main/kotlin/doma/sidim/repository/OrderRepository.kt
@@ -0,0 +1,80 @@
+package doma.sidim.repository
+
+import doma.sidim.dto.NewOrderDto
+import doma.sidim.model.Order
+import doma.sidim.model.OrderProducts
+import doma.sidim.model.Orders
+import doma.sidim.util.OrderStatus
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.time.LocalDateTime
+
+class OrderRepository(private val productRepository: ProductRepository) {
+    fun create(item: NewOrderDto, userId: Long, deliveryDate: LocalDateTime): Long {
+        var id: Long = 0
+        transaction {
+            id = Orders.insert {
+                it[orderDate] = LocalDateTime.now()
+                it[orderStatus] = OrderStatus.PENDING.ordinal
+                it[orderCost] = item.orderCost
+                it[Orders.deliveryDate] = deliveryDate
+                it[deliveryAddress] = item.deliveryAddress
+                it[Orders.userId] = userId
+            }[Orders.id]
+
+            item.productIds.forEach { productId ->
+                OrderProducts.insert {
+                    it[orderId] = id
+                    it[OrderProducts.productId] = productId
+                }
+            }
+        }
+        return id
+    }
+
+    fun read(id: Long): Order? {
+        return transaction {
+            val orderRow = Orders.select { Orders.id eq id }
+                .mapNotNull {
+                    it.toOrder()
+                }.singleOrNull()
+
+            orderRow?.let { order ->
+                val products = OrderProducts.select { OrderProducts.orderId eq id }
+                    .mapNotNull {
+                        productRepository.read(it[OrderProducts.productId])
+                    }
+
+                order.copy(products = products)
+            }
+        }
+    }
+
+    fun delete(id: Long): Boolean {
+        return transaction {
+            Orders.deleteWhere { Orders.id eq id } > 0
+        }
+    }
+
+    fun updateOrderStatus(id: Long, status: OrderStatus): Boolean {
+        return transaction {
+            Orders.update({ Orders.id eq id }) {
+                it[orderStatus] = status.ordinal
+            } > 0
+        }
+    }
+
+    private fun ResultRow.toOrder(): Order {
+        return Order(
+            id = this[Orders.id],
+            orderDate = this[Orders.orderDate],
+            orderStatus = OrderStatus.getOrderStatus(this[Orders.orderStatus]),
+            orderCost = this[Orders.orderCost],
+            deliveryDate = this[Orders.deliveryDate],
+            deliveryAddress = this[Orders.deliveryAddress],
+            products = emptyList(),
+            userId = this[Orders.userId],
+        )
+    }
+}
