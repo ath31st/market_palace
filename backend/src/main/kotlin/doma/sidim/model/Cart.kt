4eb7ee9728dbaaace5263d9339b307d5b1c6d8cc
ath31st <ath31st@yandex.ru>
2024-08-08 10:49:33 +0300

    add Cart.kt

---
+++ backend/src/main/kotlin/doma/sidim/model/Cart.kt
@@ -0,0 +1,23 @@
+package doma.sidim.model
+
+import kotlinx.serialization.Serializable
+import org.jetbrains.exposed.sql.Table
+
+@Serializable
+data class Cart(
+    val id: Long? = null,
+    val userId: Long,
+    val products: List<Product> = emptyList(),
+)
+
+object Carts : Table("carts") {
+    val id = long("id").autoIncrement()
+    val userId = long("user_id").references(Users.id).uniqueIndex()
+
+    override val primaryKey = PrimaryKey(id)
+}
+
+object CartProducts : Table("cart_products") {
+    val cartId = long("cart_id").references(Carts.id)
+    val productId = long("product_id").references(Products.id)
+}
