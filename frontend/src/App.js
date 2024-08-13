dc76470c02038fa22c553a02b19f58facbb083c9
ath31st <ath31st@yandex.ru>
2024-08-13 15:08:36 +0300

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
 import { Route, Routes } from 'react-router-dom'
 import Products from './pages/Products'
+import Layout from './components/Layout'
+import Signup from './pages/Signup'
+import NotFoundPage from './pages/NotFoundPage'
+import Login from './pages/Login'
+import PrivateRoute from './components/PrivateRoute'
 
 function App () {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Products/>}/>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Products/>}/>
+          {/*<Route path="/cart"*/}
+          {/*       element={<PrivateRoute element={<Cart/>}/>}/>*/}
+          {/*<Route path="/my-orders"*/}
+          {/*       element={<PrivateRoute element={<MyOrders/>}/>}/>*/}
+          <Route path="*" element={<NotFoundPage/>}/>
+        </Route>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/login" element={<Login/>}/>
       </Routes>
     </div>
   )
