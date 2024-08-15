8f04ec707f44ea29997e696aa092f80f5949b816
ath31st <ath31st@yandex.ru>
2024-08-15 10:16:36 +0300

    add global style in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,31 @@ import App from './App'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import store from './redux/store'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        min-height: 100vh;
+        display: grid;
+    }
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <GlobalStyle/>
         <App/>
+        <GlobalStyle/>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
