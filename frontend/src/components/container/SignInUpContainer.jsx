443963ca8e0e2558f480521481da8f3f568dd9d1
ath31st <ath31st@yandex.ru>
2024-08-15 10:18:12 +0300

    add SignInUpContainer.jsx and sign_in_up_bg.jpg for background

---
+++ frontend/src/components/container/SignInUpContainer.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const SignInUpContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background-image: url('/background/sign_in_up_bg.jpg');
+    background-size: cover;
+    background-position: center;
+`
+
+export default SignInUpContainer
