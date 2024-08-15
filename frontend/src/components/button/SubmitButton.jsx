9476396c4724d40552d120d1a3865792b4067643
ath31st <ath31st@yandex.ru>
2024-08-15 10:19:38 +0300

    add SubmitButton.jsx component

---
+++ frontend/src/components/button/SubmitButton.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const SubmitButton = styled.button`
+    width: 100%;
+    padding: 10px;
+    background-color: #32CD32;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    font-size: 18px;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #006400;
+    }
+`
+
+export default SubmitButton
