c8c37c7dc095c34b2f3e1c85c461dd29d8e32771
ath31st <ath31st@yandex.ru>
2024-08-15 10:19:29 +0300

    add SignInUpForm.jsx component

---
+++ frontend/src/components/form/SignInUpForm.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const SignInUpForm = styled.form`
+    background-color: rgba(255, 255, 255, 0.9);
+    padding: 40px;
+    border-radius: 10px;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+    max-width: 400px;
+    width: 100%;
+
+    h2 {
+        margin-bottom: 20px;
+        color: #006400;
+    }
+`
+
+export default SignInUpForm
