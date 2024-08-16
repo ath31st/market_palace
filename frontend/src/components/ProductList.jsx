8ebce21ae12db0adb921d30420b196db1ac52d6e
ath31st <ath31st@yandex.ru>
2024-08-16 13:54:37 +0300

    rework ProductList.jsx component

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -2,25 +2,36 @@ import React from 'react'
 import styled from 'styled-components'
 import Product from './Product'
 
+const ProductListWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 20px;
+    overflow: auto;
+`
+
 const ProductListContainer = styled.div`
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    grid-template-columns: repeat(5, 200px);
+    grid-auto-rows: 250px;
     gap: 20px;
-    padding: 20px;
+    height: calc(250px * 2 + 40px);
+    box-sizing: content-box;
 `
 
 const ProductList = ({ products }) => {
   return (
-    <ProductListContainer>
-      {products.map(product => (
-        <Product
-          key={product.id}
-          image={product.image}
-          title={product.title}
-          price={product.price}
-        />
-      ))}
-    </ProductListContainer>
+    <ProductListWrapper>
+      <ProductListContainer>
+        {products.map(product => (
+          <Product
+            key={product.id}
+            image={product.imageLink}
+            title={product.title}
+            price={product.price}
+          />
+        ))}
+      </ProductListContainer>
+    </ProductListWrapper>
   )
 }
 
