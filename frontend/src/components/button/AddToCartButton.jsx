68247521bded5f42f3a0e7da670af210750a1af2
ath31st <ath31st@yandex.ru>
2024-08-17 00:27:41 +0300

    add AddToCartButton.jsx component

---
+++ frontend/src/components/button/AddToCartButton.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const AddToCartButton = styled.button`
+    width: 100%;
+    padding: 5px;
+    background-color: #32CD32;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    font-size: 16px;
+    cursor: pointer;
+    margin-top: 10px;
+
+    &:hover {
+        background-color: #006400;
+    }
+`
+
+export default AddToCartButton
