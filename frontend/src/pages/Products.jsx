8c4aecda9b001e8ff84874ad88218ba89d318d3a
ath31st <ath31st@yandex.ru>
2024-08-16 11:14:41 +0300

    integrate pagination into Products.jsx page

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -3,24 +3,31 @@ import useAuth from '../hooks/useAuth'
 import axios from 'axios'
 import SearchAndSort from '../components/container/SearchAndSort'
 import ProductList from '../components/ProductList'
+import Pagination from '../components/Pagination'
 
 const Products = () => {
   const isAuthenticated = useAuth().isAuthenticated
   const apiUrl = process.env.REACT_APP_API_BASE_URL
+  const [currentPage, setCurrentPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(1)
   const [products, setProducts] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
 
   useEffect(() => {
-    const axiosInstance = axios.create({
-      baseURL: apiUrl,
-    })
+    const fetchProducts = async (page = 1) => {
+      const axiosInstance = axios.create({
+        baseURL: apiUrl,
+      })
 
-    const fetchProducts = async () => {
       try {
-        const response = await axiosInstance.get('/api/v1/products')
+        const response = await axiosInstance.get('/api/v1/products', {
+          params: { page },
+        })
         console.log(response.data)
         setProducts(response.data.items)
+        setTotalPages(response.data.totalPages)
+        setCurrentPage(response.data.currentPage)
       } catch (error) {
         console.error('Error fetching products:', error)
         setError(error)
@@ -29,14 +36,21 @@ const Products = () => {
       }
     }
 
-    fetchProducts()
-  }, [apiUrl])
+    fetchProducts(currentPage)
+  }, [currentPage, apiUrl])
 
   return (
     <div className="products">
       <SearchAndSort/>
       {loading ? <p>Loading...</p> : error ? <p>Error: {error.message}</p> :
-        <ProductList products={products}/>
+        <>
+          <ProductList products={products}/>
+          <Pagination
+            totalPages={totalPages}
+            currentPage={currentPage}
+            onPageChange={page => setCurrentPage(page)}
+          />
+        </>
       }
     </div>
   )
