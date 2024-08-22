bc4867e1cb7f93f53f34832c8d8dc86c015231bd
ath31st <ath31st@yandex.ru>
2024-08-22 10:36:27 +0300

    update style for Layout.jsx (width)

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import styled from 'styled-components'
 
-const Container = styled.main`
+const OuterContainer = styled.main`
     padding: 20px;
     min-height: calc(100vh - 140px);
     display: flex;
@@ -12,16 +12,24 @@ const Container = styled.main`
     background-image: url('/background/common_bg.jpg');
     background-size: cover;
     background-position: center;
+`
+
+const InnerContainer = styled.div`
+    max-width: 1200px;
+    width: 100%;
     margin: 0 auto;
+    flex-grow: 1;
 `
 
 const Layout = () => {
   return (
     <>
       <Header/>
-      <Container>
-        <Outlet/>
-      </Container>
+      <OuterContainer>
+        <InnerContainer>
+          <Outlet/>
+        </InnerContainer>
+      </OuterContainer>
       <Footer/>
     </>
   )
