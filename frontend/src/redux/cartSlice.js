78e1de611da0d5472db390f610ee830b26154b3a
ath31st <ath31st@yandex.ru>
2024-08-25 10:36:35 +0300

    trying to add processing cart in ProductPage.jsx v2, fix path in cartSlice.js

--- frontend/src/redux/cartSlice.js
+++ frontend/src/redux/cartSlice.js
@@ -5,7 +5,7 @@ export const fetchCart = createAsyncThunk(
   'cart/fetchCart',
   async (_, { rejectWithValue }) => {
     try {
-      const response = await axios.get('/cart')
+      const response = await axios.get('/api/v1/cart')
       return response.data
     } catch (error) {
       return rejectWithValue(error.response?.data || 'Error fetching cart')
@@ -17,7 +17,7 @@ export const addToCart = createAsyncThunk(
   'cart/addToCart',
   async (cartReq, { rejectWithValue }) => {
     try {
-      const response = await axios.post('/cart', cartReq)
+      const response = await axios.post('/api/v1/cart', cartReq)
       return response.data
     } catch (error) {
       return rejectWithValue(error.response?.data || 'Error adding to cart')
@@ -29,7 +29,7 @@ export const updateCart = createAsyncThunk(
   'cart/updateCart',
   async (cartChange, { rejectWithValue }) => {
     try {
-      const response = await axios.put('/cart', cartChange)
+      const response = await axios.put('/api/v1/cart', cartChange)
       return response.data
     } catch (error) {
       return rejectWithValue(error.response?.data || 'Error updating cart')
