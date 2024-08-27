ff824d1dc81957221f9dcaeb151e6ec0b4e5b930
ath31st <ath31st@yandex.ru>
2024-08-27 15:53:17 +0300

    rework CartPage.jsx with data from api v5, update CartPage.jsx

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react'
 import styled from 'styled-components'
-import QuantityControl from '../components/button/QuantityControl'
 import { fetchCart, updateCart } from '../redux/cartSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import SubmitButton from '../components/button/SubmitButton'
+import CartItem from '../components/CartItem'
 
 const CartContainer = styled.div`
     display: flex;
@@ -12,55 +12,6 @@ const CartContainer = styled.div`
 
 const CartItems = styled.div`
     flex: 2;
-`
-
-const CartItem = styled.div`
-    display: flex;
-    justify-content: space-between;
-    padding: 10px;
-    border: 1px solid #ddd;
-    margin-bottom: 10px;
-    background-color: #f9f9f9;
-    border-radius: 5px;
-`
-
-const ProductInfo = styled.div`
-    display: flex;
-`
-
-const ProductImage = styled.img`
-    width: 100px;
-    height: 100px;
-    object-fit: cover;
-    border: 1px solid #ddd;
-    margin-right: 20px;
-    border-radius: 5px;
-`
-
-const ProductDetails = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-`
-
-const ProductTitle = styled.p`
-    font-size: 20px;
-    font-weight: bold;
-    margin: 0;
-    color: green;
-`
-
-const ProductDescription = styled.p`
-    font-size: 16px;
-    margin: 0;
-    color: green;
-`
-
-const ProductPrice = styled.div`
-    display: flex;
-    align-items: center;
-    font-size: 20px;
-    color: green;
 `
 
 const CartSummary = styled.div`
@@ -101,40 +52,35 @@ const CartPage = () => {
       }
       console.log(productUpdate)
       dispatch(updateCart(productUpdate))
+      dispatch(fetchCart())
     }
   }
 
   return (
     <CartContainer>
       <CartItems>
-        {cartItems.map(item => (
-          <CartItem key={item.id}>
-            <ProductInfo>
-              <ProductImage src={item.imageLink} alt={item.title}/>
-              <ProductDetails>
-                <ProductTitle>{item.title}</ProductTitle>
-                <ProductDescription>{item.smallDescription}</ProductDescription>
-              </ProductDetails>
-            </ProductInfo>
-            <ProductPrice>${item.price}</ProductPrice>
-            <QuantityControl
-              quantity={item.quantity}
-              onIncrement={() => handleQuantityChange(item.id, 1)}
-              onDecrement={() => handleQuantityChange(item.id, -1)}
+        {cartItems && cartItems.length > 0 ? (
+          cartItems.map(item => (
+            <CartItem
+              key={item.id}
+              item={item}
+              handleQuantityChange={handleQuantityChange}
             />
-          </CartItem>
-        ))}
+          ))
+        ) : (
+          <p>Your cart is empty</p>
+        )}
       </CartItems>
       <CartSummary>
         <SummaryItem>
           <span>Product count:</span>
-          <span>{cartItems.reduce((total, item) => total + item.quantity,
-            0)}</span>
+          <span>{cartItems ? cartItems.reduce(
+            (total, item) => total + item.quantity, 0) : 0}</span>
         </SummaryItem>
         <SummaryItem>
           <span>Total price:</span>
-          <span>${cartItems.reduce(
-            (total, item) => total + item.price * item.quantity, 0)}</span>
+          <span>${cartItems ? cartItems.reduce(
+            (total, item) => total + item.price * item.quantity, 0) : 0}</span>
         </SummaryItem>
         <SubmitButton>Create order</SubmitButton>
       </CartSummary>
