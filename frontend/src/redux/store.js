e03fe726a787c4c2be14bda65e8be7676377826d
ath31st <ath31st@yandex.ru>
2024-08-28 12:49:32 +0300

    update store.js with order reducer

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './authSlice'
 import cartReducer from './cartSlice'
+import orderSlice from './orderSlice'
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     cart: cartReducer,
+    order: orderSlice,
   },
 })
 
