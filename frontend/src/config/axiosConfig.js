3e3ab260a074c6fe8c034b44d3b656dc04c068c4
ath31st <ath31st@yandex.ru>
2024-08-28 10:00:44 +0300

    rework axiosConfig.js

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -1,31 +1,31 @@
 import axios from 'axios'
-import store from '../redux/store'
-import { performLogout } from '../redux/authSlice'
 
 const apiUrl = process.env.REACT_APP_API_BASE_URL
 
-axios.defaults.baseURL = apiUrl
+const axiosInstance = axios.create({
+  baseURL: apiUrl,
+})
 
-axios.interceptors.request.use(
-  config => {
-    const token = store.getState().auth.token
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('jwtToken')
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }
     return config
   },
-  error => Promise.reject(error),
+  (error) => Promise.reject(error),
 )
 
-axios.interceptors.response.use(
-  response => response,
-  error => {
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
     if (error.response && error.response.status === 401) {
-      store.dispatch(performLogout())
+      localStorage.removeItem('jwtToken')
       window.location.href = '/login'
     }
     return Promise.reject(error)
   },
 )
 
-export default axios
+export default axiosInstance
