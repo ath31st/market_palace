84473b438a7eff950bde55582561fb25a576e257
ath31st <ath31st@yandex.ru>
2024-08-29 09:02:49 +0300

    update Cart.kt with mapping methods

--- backend/src/main/kotlin/doma/sidim/model/Cart.kt
+++ backend/src/main/kotlin/doma/sidim/model/Cart.kt
@@ -1,7 +1,6 @@
 package doma.sidim.model
 
 import doma.sidim.dto.CartDto
-import doma.sidim.dto.ProductInCartDto
 import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.sql.Table
 
@@ -16,20 +15,7 @@ fun Cart.toCartDto(): CartDto {
     return CartDto(
         this.id,
         products = this.products.entries.map { entry ->
-            val smallDescription = if (entry.key.description.length > 100) {
-                entry.key.description.substring(0, 100) + "..."
-            } else {
-                entry.key.description
-            }
-
-            ProductInCartDto(
-                entry.key.id!!,
-                entry.key.title,
-                entry.value,
-                smallDescription,
-                entry.key.imageLink,
-                entry.key.price
-            )
+            entry.key.toProductInCartDto(entry.value)
         }
     )
 }
