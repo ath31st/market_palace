af41dfc5aef1faef811d6e0e25d9f9472bb364c5
ath31st <ath31st@yandex.ru>
2024-08-29 09:03:40 +0300

    update Order.kt with mapping methods

--- backend/src/main/kotlin/doma/sidim/model/Order.kt
+++ backend/src/main/kotlin/doma/sidim/model/Order.kt
@@ -1,5 +1,6 @@
 package doma.sidim.model
 
+import doma.sidim.dto.OrderDto
 import doma.sidim.util.OrderStatus
 import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
@@ -18,6 +19,17 @@ data class Order(
     val userId: Long,
     val products: Map<Product, Int>
 )
+
+fun Order.toOrderDto() = OrderDto(
+    this.id!!,
+    this.orderDate.toString(),
+    this.deliveryDate.toString(),
+    this.deliveryAddress,
+    this.orderCost,
+    this.orderStatus.displayName(),
+    this.products.entries.map { it.key.toProductInOrderDto(it.value) }
+)
+
 
 object Orders : Table("orders") {
     val id = long("id").autoIncrement()
